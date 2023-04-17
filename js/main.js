393e8e2fdae57c26ff87f9a724bc497775f8c2e8
jair <jair.ibarra.p@gmail.com>
2023-04-17 00:08:35 -0600

    Se agregó el archivo global.css que contiene el grid general, se enlazaron las páginas

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ let enlacesNav = document.getElementById("enlacesNav");
 let botonesNav = document.getElementById("botonesNav");
 
 let enlaces = `
-                <a href="./About.html" class="letrasNav" style="padding-left: 20px;">Acerca de</a>
-                <a href="./proyectos.html" class="letrasNav" style="padding-left: 20px;">Mis proyectos</a>
+                <a href="./index.html" class="letrasNav" >Inicio</a>
+                <a href="./About.html" class="letrasNav" >Acerca de</a>
+                <a href="./proyecto.html" class="letrasNav" >Mis proyectos</a>
                 `
 let btnNav = `
             <a href="https://github.com/Ja1roh" class="btnNav" target="_blank"><img src="./src/gitHubLogo.png" alt="gitHub" id="gitHub"></a>
