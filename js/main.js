323d36a8fe7747ef906a9da38ca29bc9985e48b0
jair <jair.ibarra.p@gmail.com>
2023-04-16 22:59:21 -0600

    Se agregó el grid

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let enlacesNav = document.getElementById("enlacesNav");
 let botonesNav = document.getElementById("botonesNav");
 
-let enlaces = `<a href="./About.html" class="letrasNav" style="padding-left: 20px;">Acerca de</a>
+let enlaces = `
+                <a href="./About.html" class="letrasNav" style="padding-left: 20px;">Acerca de</a>
                 <a href="./proyectos.html" class="letrasNav" style="padding-left: 20px;">Mis proyectos</a>
-                <a href="./contacto.html" class="letrasNav" style="padding-left: 20px;">Contacto</a>
                 `
 let btnNav = `
-            <a href="https://github.com/Ja1roh" target="_blank"><img src="./src/gitHubLogo.png" alt="gitHub" id="gitHub"></a>
-            <a href="https://www.linkedin.com/in/jair-ibarra-partida/" target="_blank"><img src="./src/linkedInLogo.png" alt="lnkIn" id="lnkIn"></a>
+            <a href="https://github.com/Ja1roh" class="btnNav" target="_blank"><img src="./src/gitHubLogo.png" alt="gitHub" id="gitHub"></a>
+            <a href="https://www.linkedin.com/in/jair-ibarra-partida/" class="btnNav" target="_blank"><img src="./src/linkedInLogo.png" alt="lnkIn" id="lnkIn"></a>
 `
 
 window.addEventListener("load", function (event) {
